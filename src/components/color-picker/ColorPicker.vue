29fe5703eac5ecaeddd6bf9d34ed8bd2ece53242
wang-joy <wang_joy13@163.com>
2018-12-04 21:21:55 +0800

    添加ColorSpan组件

--- src/components/color-picker/ColorPicker.vue
+++ src/components/color-picker/ColorPicker.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="color-picker">
-
+    <color-span class="color-span"></color-span>
   </div>
 </template>
 
 <script>
+import ColorSpan from './ColorSpan'
 export default {
-
+  components: {ColorSpan}
 }
 </script>
 
@@ -14,5 +15,10 @@ export default {
   .color-picker{
     width: 400px;
     height: 300px;
+    border: 1px solid #ccc;
+  }
+  .color-picker .color-span{
+    width: 350px;
+    height: 240px;
   }
 </style>
---
+++ src/components/color-picker/ColorSpan.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="color-span">
+    <div class="color-span-white"></div>
+    <div class="color-span-black"></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  .color-span{
+    position: relative;
+  }
+  .color-span .color-span-white, .color-span .color-span-black{
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+  .color-span .color-span-white{
+    background: linear-gradient(90deg,#fff,hsla(0,0%,100%,0));
+  }
+  .color-span .color-span-black{
+    background: linear-gradient(0deg,#000,transparent);
+  }
+</style>
