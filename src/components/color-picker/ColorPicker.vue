86ac11df381a71c22b4ef0f815b44a5f6c16cd80
wang-joy <wang_joy13@163.com>
2018-12-04 20:52:32 +0800

    初始化项目

---
+++ src/components/color-picker/ColorPicker.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="color-picker">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .color-picker{
+    width: 400px;
+    height: 300px;
+  }
+</style>
